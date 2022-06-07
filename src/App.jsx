2299cb9812baf23706fead843796e82d82e08813
duvanca <73137805+duvanca@users.noreply.github.com>
2022-06-06 21:06:02 -0500

    tercer cambio

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,84 @@ import CardsWeather from './components/CardsWeather'
 import React from 'react'
 import Loading from './components/Loading'
 import {useState,useEffect } from 'react'
+import axios from 'axios'
 
 
 
 
 function App() {
-   const[isLoading, setIsLoading]=useState(true)
+  const [latLon, setLatlon]=useState()
+  const [weather, setWeather]=useState()
+  const[loading, setLoading]=useState(true)
+  
+  const gradosCelcius = "°C"
+  const gradosfahrenheit = "°F"
+  const changes = Math.floor(weather?.main.temp-273.15) 
+  console.log(changes)
+  
+  const newChange = Math.round(changes*(9/5)+32)
+  
+  const [change, setchangue]=useState(true) 
+  const changetemp =()=>setchangue(!change)
+ 
 
-        useEffect(()=>{
-          setTimeout(()=>{
-            setIsLoading(false)
-          },2000)
-        }) 
+  useEffect(()=>{
+    const success= pos=>{
+     
+      const lon = pos.coords.longitude
+      const lat = pos.coords.latitude
+      setLatlon({lon, lat})
+    }
+  navigator.geolocation.getCurrentPosition(success)
+},[])
+   
+  
+    useEffect(()=>{
+        if(latLon !== undefined){
+          const api = "030e5636e8a867553f884e3a9b788a94"
+          const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latLon?.lat}&lon=${latLon?.lon}&appid=${api}`
+          
+          
+          axios.get(url)
+
+          .then(res=>{setWeather(res.data)
+          setLoading(false)
+          })
+
+          .catch(err=>console.log(err))
+      }
+      } ,[latLon])
+
+      console.log(weather)
+
+     
+
+      const hour= new Date().getHours()
+      const minute= new Date().getMinutes()
+      const day = new Date().getDate ()
+      const month = new Date().getMonth()+1
+      const year = 22
+     
 
  return (
     <div className="App">
-    {isLoading==true?
+    {loading?
     <Loading/>:
-    <CardsWeather/>
+    <CardsWeather
+    weather={weather}
+    hour={hour}
+    minute={minute}
+    day={day}
+    month={month}
+    year={year}
+    gradosCelcius={gradosCelcius}
+    gradosfahrenheit={gradosfahrenheit}
+    changes={changes}
+    change={change}
+    newChange={newChange}
+    changetemp={changetemp}
+
+    />
     }
 
     
--- src/components/CardsWeather.jsx
+++ src/components/CardsWeather.jsx
@@ -1,55 +1,11 @@
 import React from 'react'
 import sol from "../assets/img/sol.svg"
 import luna from "../assets/img/luna.svg"
-import {useState,useEffect } from 'react'
-import axios from 'axios'
 
-const CardsWeather = () => {
-    const [latLon, setLatlon]=useState()
-    const [weather, setWeather]=useState()
-    
-    const gradosCelcius = "°C"
-    const gradosfahrenheit = "°F"
-    const changes = Math.floor(weather?.main.temp-273.15) 
-    console.log(changes)
-    
-    const newChange = Math.round(changes*(9/5)+32)
-    
-    const [change, setchangue]=useState(true) 
-    const changetemp =()=>setchangue(!change)
+
+
+const CardsWeather = ({weather, minute, day, year, hour,month,gradosCelcius,gradosfahrenheit,change,changes,newChange,changetemp}) => {
    
-
-    useEffect(()=>{
-      const success= pos=>{
-       
-        const lon = pos.coords.longitude
-        const lat = pos.coords.latitude
-        setLatlon({lon, lat})
-      }
-    navigator.geolocation.getCurrentPosition(success)
-},[])
-     
-    
-      useEffect(()=>{
-          if(latLon !== undefined){
-            const api = "030e5636e8a867553f884e3a9b788a94"
-            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latLon?.lat}&lon=${latLon?.lon}&appid=${api}`
-            axios.get(url)
-            .then(res=>setWeather(res.data))
-            .catch(err=>console.log(err))
-        }
-        } ,[latLon])
-
-        console.log(weather)
-
-       
-
-        const hour= new Date().getHours()
-        const minute= new Date().getMinutes()
-        const day = new Date().getDate ()
-        const month = new Date().getMonth()+1
-        const year = 22
-       
         	
  return (
     <div className='Weatherapp'>
